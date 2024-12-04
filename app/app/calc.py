dadc97b1e64d9ce7d837dea9afd225166e1cc224
Rohan-Biddano <rohan.rajkumar@biddano.com>
2024-12-04 16:21:41 +0530

    added changes in docker file

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,3 +5,7 @@ Calculator functions
 def add(x,y):
     """Add x nad y and return result."""
     return x + y
+
+def subtract(x, y):
+    """Subtract y from x and return result."""
+    return x - y
