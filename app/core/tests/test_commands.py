d0dd6c6c8fae13f9220c545767143a15e87d700e
Rohan-Biddano <rohan.rajkumar@biddano.com>
2025-01-02 10:44:43 +0530

    added changes upto lec 55

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -12,7 +12,7 @@ from django.test import SimpleTestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-    """Test commands."""
+    """Test commands.""" 
 
     def test_wait_for_db_ready(self,patched_check):
         """Test waiting for databse if database ready."""
@@ -21,7 +21,6 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
-    
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting operationalError."""
